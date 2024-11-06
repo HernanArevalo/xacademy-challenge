00cf0767b64f1d81632a704d9e749df1651e4f50
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-05 21:50:29 -0300

    add playerPositions const

---
+++ frontend/src/app/core/utils/playerPositions.ts
@@ -0,0 +1,6 @@
+export const playerPositions = {
+  Goalkeeper: ['GK'],
+  Defense: ['DF', 'CB', 'RB', 'LB', 'RWB', 'LWB'],
+  Midfield: ['MF', 'CM', 'CDM', 'CAM', 'RM', 'LM'],
+  Forward: ['FW', 'ST', 'CF', 'RW', 'LW', 'SW']
+};
