951fff60abf537170fcd18d6fb0738f5d4eb9c23
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-05 21:46:48 -0300

    format document

--- backend/src/providers/playerProvider.js
+++ backend/src/providers/playerProvider.js
@@ -1,38 +1,41 @@
-const { FemalePlayer, MalePlayer } = require("../models");
-const { Sequelize } = require('sequelize');
+const { FemalePlayer, MalePlayer } = require('../models');
+const { Sequelize, where, Op } = require('sequelize');
 
-const createPlayer = async (userOptions)=>{
-  try{
+const createPlayer = async (userOptions) => {
+  try {
     const newUser = await Player.create(userOptions);
     return newUser;
-  }catch (error){
+  } catch (error) {
     throw new Error(error);
   }
 };
 
-const getPlayer = async(id, genre) => {
-  try{
+const getPlayer = async (genre, player_id) => {
+  try {
     let user;
-    if (genre == 'female') {
-      user = await FemalePlayer.findByPk(id);
-    }else{
-      user = await MalePlayer.findByPk(id);
-    }
+    const model = genre === 'female' ? FemalePlayer : MalePlayer;
+
+    user = await model.findOne({
+      where: {
+        id: player_id,
+      },
+    });
+
     if (user) {
       return user;
-    }else{
+    } else {
       throw new Error(`User not found`);
     }
-  }catch (error){
+  } catch (error) {
     throw new Error(error);
   }
 };
 
 const getPlayers = async (genre, page = 1, limit = 20, filters) => {
-  console.log({genre, page, limit, filters});
   const offset = (page - 1) * limit;
+
   try {
-    let whereClause = {};
+    const whereClause = {};
 
     if (filters.club_name) {
       whereClause.club_name = filters.club_name;
@@ -45,50 +48,56 @@ const getPlayers = async (genre, page = 1, limit = 20, filters) => {
     }
     if (filters.player_positions) {
       whereClause.player_positions = {
-        [Op.like]: `%${filters.player_positions}%`
+        [Op.like]: `%${filters.player_positions}%`,
       };
     }
 
-    let players;
     const playerModel = genre === 'female' ? FemalePlayer : MalePlayer;
 
-    players = await playerModel.findAll({
+    const players = await playerModel.findAll({
       where: whereClause,
       order: [
         ['fifa_version', 'DESC'],
-        ['overall', 'DESC']
+        ['overall', 'DESC'],
       ],
       limit: Number(limit),
-      offset: Number(offset)
+      offset: Number(offset),
     });
 
-    if (players) {
-      return players;
-    } else {
+    if (!players.length) {
       throw new Error('Players not found');
     }
+
+    return players;
   } catch (error) {
-    throw new Error(error);
+    console.error('Error in getPlayers:', error.message);
+    throw new Error('Error retrieving players');
   }
 };
 
 const getClubs = async (genre) => {
   try {
     let clubs = [];
-    
+
     if (genre === 'female') {
       clubs = await FemalePlayer.findAll({
-        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('club_name')), 'club_name']],
+        attributes: [
+          [Sequelize.fn('DISTINCT', Sequelize.col('club_name')), 'club_name'],
+        ],
         raw: true,
       });
     } else {
       clubs = await MalePlayer.findAll({
-        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('club_name')), 'club_name']],
+        attributes: [
+          [Sequelize.fn('DISTINCT', Sequelize.col('club_name')), 'club_name'],
+        ],
         raw: true,
       });
     }
 
-    return clubs.map(club => club.club_name).sort((a, b) => a.localeCompare(b));
+    return clubs
+      .map((club) => club.club_name)
+      .sort((a, b) => a.localeCompare(b));
   } catch (error) {
     console.error('Error fetching unique club names:', error);
     throw new Error(error);
@@ -97,46 +106,55 @@ const getClubs = async (genre) => {
 const getNations = async (genre) => {
   try {
     let nations = [];
-    
+
     if (genre === 'female') {
       nations = await FemalePlayer.findAll({
-        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('nationality_name')), 'nationality_name']],
+        attributes: [
+          [
+            Sequelize.fn('DISTINCT', Sequelize.col('nationality_name')),
+            'nationality_name',
+          ],
+        ],
         raw: true,
       });
     } else {
       nations = await MalePlayer.findAll({
-        attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('nationality_name')), 'nationality_name']],
+        attributes: [
+          [
+            Sequelize.fn('DISTINCT', Sequelize.col('nationality_name')),
+            'nationality_name',
+          ],
+        ],
         raw: true,
       });
     }
 
-    return nations.map(nation => nation.nationality_name).sort((a, b) => a.localeCompare(b));
+    return nations
+      .map((nation) => nation.nationality_name)
+      .sort((a, b) => a.localeCompare(b));
   } catch (error) {
     console.error('Error fetching unique club names:', error);
     throw new Error(error);
   }
 };
 
-const updateUser = async (id, userOptions)=>{
-  try{
-    await getUser(id)
-    const res = await User.update(userOptions,
-      { where: { id },
-        returning: true
-      },
-);
+const updateUser = async (id, userOptions) => {
+  try {
+    await getUser(id);
+    const res = await User.update(userOptions, {
+      where: { id },
+      returning: true,
+    });
     return User.findByPk(id);
-  }catch (error){
+  } catch (error) {
     throw new Error(error);
   }
 };
 
-const deleteUser = async (id)=>{
-  try{
-    return await User.destroy( { where: { id } },
-
-);
-  }catch (error){
+const deleteUser = async (id) => {
+  try {
+    return await User.destroy({ where: { id } });
+  } catch (error) {
     throw new Error(error);
   }
 };
@@ -148,6 +166,5 @@ module.exports = {
   getClubs,
   getNations,
   updateUser,
-  deleteUser
-}
-
+  deleteUser,
+};
