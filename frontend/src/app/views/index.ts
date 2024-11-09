9ea893d2261513bd1097664cd204100f80159b69
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-09 20:47:56 -0300

    update exports in index

--- frontend/src/app/views/index.ts
+++ frontend/src/app/views/index.ts
@@ -1,1 +1,3 @@
+export { PlayerComponent } from './player/player.component'
 export { EditPlayerComponent } from './player/edit/edit.component'
+export { PlayersComponent } from './players/genre/players.component'
