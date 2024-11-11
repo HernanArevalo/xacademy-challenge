943c2b64caac08f5dfb839549c3640e4faa65bfc
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-11 06:52:18 -0300

    use auth interceptors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,27 +1,39 @@
 import { Routes } from '@angular/router';
-import { PlayersComponent, PlayerComponent, EditPlayerComponent } from './views';
+import { PlayersComponent, PlayerComponent, EditPlayerComponent, LoginComponent, RegisterComponent } from './views';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { 
+    path: 'login', 
+    component: LoginComponent,
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent,
+  },
+  { 
     path: 'players/:genre', 
     component: PlayersComponent,
+    canActivate: [AuthGuard]
   },
   { 
     path: 'players/:genre/:player_id', 
-    component: PlayerComponent
+    component: PlayerComponent,
+    canActivate: [AuthGuard]
   },
   { 
     path: 'players/:genre/:player_id/edit', 
-    component: EditPlayerComponent
+    component: EditPlayerComponent,
+    canActivate: [AuthGuard]
   },
   { 
     path: '', 
     redirectTo: 'players/male',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   { 
     path: '**', 
     redirectTo: 'players/male', 
-    pathMatch: 'full' 
+    pathMatch: 'full',
   }
 ];
