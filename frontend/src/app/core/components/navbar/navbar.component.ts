f8b8569c42d645b1162339bc562805824e821e14
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-11 06:53:28 -0300

    implements user players data in navbar buttons

--- frontend/src/app/core/components/navbar/navbar.component.ts
+++ frontend/src/app/core/components/navbar/navbar.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/user/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  
+export class NavbarComponent implements OnInit {
+  malePlayerId: number | null = null;
+  femalePlayerId: number | null = null;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.getUserData().subscribe(user => {
+      if (user) {
+        this.malePlayerId = user.male_player_id;
+        this.femalePlayerId = user.female_player_id;
+      }
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
