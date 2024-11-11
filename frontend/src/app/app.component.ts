6e027becef25ba95d15194e6c2fef96b4e1a0059
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-11 06:51:51 -0300

    show navbar only if is logged

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 import { NavbarComponent } from '@/core/components';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, NavbarComponent ],
+  imports: [ RouterOutlet, NavbarComponent, CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'AppComponent';
+export class AppComponent implements OnInit{
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(event => {
+      this.showNavbar = !['/login', '/register'].includes(this.router.url);
+    });
+  }
+
+  ngOnInit() {
+  }
 };
